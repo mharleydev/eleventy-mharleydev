bdfdba3546cd2010b4edbbaf05d56f417cde8648
Michael Harley <michael@mharley.dev>
2020-09-02 11:16:20 -0500

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+    return {
+        // These are all optional, defaults are shown:
+        dir: {
+          input: ".",
+          includes: "_includes",
+          data: "_data",
+          output: "../mharley.dev"
+        }
+      };
+    
+}
--- posts/hello-world.md
+++ posts/hello-world.md
@@ -1,6 +1,6 @@
 ---
-layout: layout.liquid
 pageTitle: Hello world
+Date: 2020-08-24
 ---
 
 Hello world. This is a paragraph of text.
