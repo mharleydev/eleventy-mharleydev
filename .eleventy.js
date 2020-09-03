b0659bb4c50df28b370c7a09247f108f1cb51452
Michael Harley <michael@mharley.dev>
2020-09-03 13:28:51 -0500

    Add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
-    return {
-        // These are all optional, defaults are shown:
-        dir: {
-          input: ".",
-          includes: "_includes",
-          data: "_data",
-          output: "../mharley.dev"
-        }
-      };
-    
+  eleventyConfig.addPlugin(pluginRss);
+  return {
+      // These are all optional, defaults are shown:
+      dir: {
+        input: ".",
+        includes: "_includes",
+        data: "_data",
+        output: "../mharley.dev"
+      }
+    };   
 }
